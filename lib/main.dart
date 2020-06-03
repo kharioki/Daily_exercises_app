53d0079f088da0b412ad0459392388e646112a15
kharioki <antolisah@gmail.com>
2020-06-03 04:24:39 +0300

    added homescreen exercise grid

--- lib/main.dart
+++ lib/main.dart
@@ -81,11 +81,100 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                   ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        CategoryCard(
+                          svgImage: 'assets/icons/Hamburger.svg',
+                          title: 'Diet Recommendation',
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          svgImage: 'assets/icons/Excercises.svg',
+                          title: 'Kegel Exercises',
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          svgImage: 'assets/icons/Meditation.svg',
+                          title: 'Meditation',
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          svgImage: 'assets/icons/yoga.svg',
+                          title: 'Yoga',
+                          press: () {},
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String svgImage;
+  final String title;
+  final Function press;
+
+  const CategoryCard({
+    Key key,
+    this.svgImage,
+    this.title,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(13),
+      child: Container(
+        // padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(13),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 17),
+              blurRadius: 17,
+              spreadRadius: -23,
+              color: kShadowColor,
+            ),
+          ],
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: press,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Spacer(),
+                  SvgPicture.asset(svgImage),
+                  Spacer(),
+                  Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(fontSize: 15),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
