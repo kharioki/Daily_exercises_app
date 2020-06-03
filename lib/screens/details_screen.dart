e8ac42e814d1132fffd1311a270d4784035f8f44
kharioki <antolisah@gmail.com>
2020-06-03 06:40:48 +0300

    session widget

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -34,7 +34,7 @@ class DetailsScreen extends StatelessWidget {
                     style: Theme.of(context)
                         .textTheme
                         .headline6
-                        .copyWith(fontWeight: FontWeight.w800),
+                        .copyWith(fontWeight: FontWeight.w900),
                   ),
                   SizedBox(height: 10),
                   Text(
@@ -47,7 +47,65 @@ class DetailsScreen extends StatelessWidget {
                     child: Text(
                         'Live happier and healthier by learning the fundamentals of meditation and mindfulness.'),
                   ),
-                  SearchBar(),
+                  SizedBox(
+                    width: size.width * .5, // 50% of width
+                    child: SearchBar(),
+                  ),
+                  Wrap(
+                    spacing: 20,
+                    runSpacing: 20,
+                    children: <Widget>[
+                      SessionCard(
+                        sessionNum: 1,
+                        isDone: true,
+                        press: () {},
+                      ),
+                      SessionCard(
+                        sessionNum: 2,
+                        press: () {},
+                      ),
+                      SessionCard(
+                        sessionNum: 3,
+                        press: () {},
+                      ),
+                      SessionCard(
+                        sessionNum: 4,
+                        press: () {},
+                      ),
+                      SessionCard(
+                        sessionNum: 5,
+                        press: () {},
+                      ),
+                      SessionCard(
+                        sessionNum: 6,
+                        press: () {},
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Meditation',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    height: 90,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(13),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 17),
+                          blurRadius: 23,
+                          spreadRadius: -13,
+                          color: kShadowColor,
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -55,5 +113,75 @@ class DetailsScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class SessionCard extends StatelessWidget {
+  final int sessionNum;
+  final bool isDone;
+  final Function press;
+
+  const SessionCard({
+    Key key,
+    this.sessionNum,
+    this.isDone = false,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraint) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(13),
+        child: Container(
+          width: constraint.maxWidth / 2 -
+              10, //provides us with available max width for this widget
+          // padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(13),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 17),
+                blurRadius: 23,
+                spreadRadius: -13,
+                color: kShadowColor,
+              ),
+            ],
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: press,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 42,
+                      width: 43,
+                      decoration: BoxDecoration(
+                        color: isDone ? kBlueColor : Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: kBlueColor),
+                      ),
+                      child: Icon(
+                        Icons.play_arrow,
+                        color: isDone ? Colors.white : kBlueColor,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Session $sessionNum',
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
