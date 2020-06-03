1f512dbd9afde9a0a4fb14375d415888f7084ec5
kharioki <antolisah@gmail.com>
2020-06-03 05:40:44 +0300

    search bar widget

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meditation_app/constants.dart';
+import 'package:meditation_app/widgets/search_bar.dart';
 
 class DetailsScreen extends StatelessWidget {
   @override
@@ -20,19 +21,35 @@ class DetailsScreen extends StatelessWidget {
             ),
           ),
           SafeArea(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: size.height * .05,
-                ),
-                Text(
-                  'Meditation',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6
-                      .copyWith(fontWeight: FontWeight.w800),
-                ),
-              ],
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: size.height * .05,
+                  ),
+                  Text(
+                    'Meditation',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(fontWeight: FontWeight.w800),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '3-10 Min Course',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: size.width * .6, // 60% of total width
+                    child: Text(
+                        'Live happier and healthier by learning the fundamentals of meditation and mindfulness.'),
+                  ),
+                  SearchBar(),
+                ],
+              ),
             ),
           ),
         ],
