15a9358a06800ec3b6486af0c697c25fce936feb
kharioki <antolisah@gmail.com>
2020-06-03 06:54:26 +0300

    current playing

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:meditation_app/constants.dart';
+import 'package:meditation_app/widgets/bottom_nav_bar.dart';
 import 'package:meditation_app/widgets/search_bar.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -7,6 +9,7 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
       body: Stack(
         children: <Widget>[
           Container(
@@ -23,90 +26,119 @@ class DetailsScreen extends StatelessWidget {
           SafeArea(
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                    height: size.height * .05,
-                  ),
-                  Text(
-                    'Meditation',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline6
-                        .copyWith(fontWeight: FontWeight.w900),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    '3-10 Min Course',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    width: size.width * .6, // 60% of total width
-                    child: Text(
-                        'Live happier and healthier by learning the fundamentals of meditation and mindfulness.'),
-                  ),
-                  SizedBox(
-                    width: size.width * .5, // 50% of width
-                    child: SearchBar(),
-                  ),
-                  Wrap(
-                    spacing: 20,
-                    runSpacing: 20,
-                    children: <Widget>[
-                      SessionCard(
-                        sessionNum: 1,
-                        isDone: true,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 2,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 3,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 4,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 5,
-                        press: () {},
-                      ),
-                      SessionCard(
-                        sessionNum: 6,
-                        press: () {},
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Meditation',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline6
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 20),
-                    height: 90,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(13),
-                      boxShadow: [
-                        BoxShadow(
-                          offset: Offset(0, 17),
-                          blurRadius: 23,
-                          spreadRadius: -13,
-                          color: kShadowColor,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: size.height * .05,
+                    ),
+                    Text(
+                      'Meditation',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4
+                          .copyWith(fontWeight: FontWeight.w900),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      '3-10 Min Course',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: size.width * .6, // 60% of total width
+                      child: Text(
+                          'Live happier and healthier by learning the fundamentals of meditation and mindfulness.'),
+                    ),
+                    SizedBox(
+                      width: size.width * .5, // 50% of width
+                      child: SearchBar(),
+                    ),
+                    Wrap(
+                      spacing: 20,
+                      runSpacing: 20,
+                      children: <Widget>[
+                        SessionCard(
+                          sessionNum: 1,
+                          isDone: true,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 2,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 3,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 4,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 5,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          sessionNum: 6,
+                          press: () {},
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    SizedBox(height: 20),
+                    Text(
+                      'Meditation',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: EdgeInsets.all(10),
+                      height: 90,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          SvgPicture.asset(
+                              'assets/icons/Meditation_women_small.svg'),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  'Basics 2',
+                                  style: Theme.of(context).textTheme.subtitle1,
+                                ),
+                                Text(
+                                  'Start to deepen your practice',
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: SvgPicture.asset('assets/icons/Lock.svg'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
